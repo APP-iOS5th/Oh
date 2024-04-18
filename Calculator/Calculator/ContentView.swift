4f37899fdef65f34754e15b443428dab9f1e16c9
wotjs020708 <wotjs020708@gmail.com>
2024-04-18 14:08:04 +0900

    초기 UI

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -7,12 +7,159 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("2+3")
+            // 첫번째 줄
+            HStack{
+                Button{
+                    
+                } label: {
+                    Text("7")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button{
+                    
+                } label: {
+                    Text("8")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button{
+                    
+                } label: {
+                    Text("9")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button {
+                    
+                } label: {
+                    Text("/")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+
+            }
+            // 두번째 줄
+            HStack{
+                Button{
+                    
+                } label: {
+                    Text("4")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button{
+                    
+                } label: {
+                    Text("5")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button{
+                    
+                } label: {
+                    Text("6")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button {
+                    
+                } label: {
+                    Text("*")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+            }
+            // 세번째 줄
+            HStack{
+                Button{
+                    
+                } label: {
+                    Text("1")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button{
+                    
+                } label: {
+                    Text("2")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button{
+                    
+                } label: {
+                    Text("3")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button {
+                    
+                } label: {
+                    Text("+")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+
+            }
+            // 네번째 줄
+            HStack{
+                Button{
+                    
+                } label: {
+                    Text(".")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button{
+                    
+                } label: {
+                    Text("0")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                Button{
+                    
+                } label: {
+                    Text("C")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("+")
+                        .bold()
+                        .padding(19)
+                        .border(Color.black)
+                }
+                
+                
+            }
             
         }
-        .padding()
+        
     }
 }
 
