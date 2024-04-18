8ddc99196988fbf28eb0d8b76d246f1ca9f05a45
kyhlsd <kmyghn@gmail.com>
2024-04-18 13:22:57 +0900

    Initial Commit

---
+++ Project1_lab5/Project1_lab5App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1_lab5App.swift
+//  Project1_lab5
+//
+//  Created by 김영훈 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Project1_lab5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
