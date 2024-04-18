12b5db42621ecc5fc270015fabf1e339fc42db0e
wotjs020708 <wotjs020708@gmail.com>
2024-04-18 15:49:35 +0900

    calculator UI

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputValue = "0"
+    
+    let buttons = [
+    ["7","8","9", "/"],
+    ["4","5","6", "*"],
+    ["1","2","3", "-"],
+    [".","0","C", "+"],
+    ["="]
+    ]
     
     var body: some View {
         VStack{
+            Text(inputValue)
+                .frame(maxWidth: .infinity,alignment: .trailing)
+                .font(.system(size: 48))
+                .padding()
+                .background(Color.gray.opacity(0.2))
+            ForEach(buttons, id: \.self) { row in
+                HStack{
+                    ForEach(row, id: \.self) { buttonChar in
+                        Button(action: {
+                            print(buttonChar)
+                        }){
+                            Text(buttonChar)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .font(.system(size: 48))
+                                .foregroundStyle(buttonChar == "C" ? Color.red : Color.blue)
+                                .border(Color.gray, width: 1)
+                        }
+                        
+                    }
+                }
+            }
             
         }
     }
