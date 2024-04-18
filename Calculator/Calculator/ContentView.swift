b517d09e29b46b5834ebbaab4cefff889e82c947
wotjs020708 <wotjs020708@gmail.com>
2024-04-18 13:44:37 +0900

    add .gitignore, 프로젝트 생성

---
+++ Calculator/Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by 어재선 on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
