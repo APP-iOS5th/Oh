0237ff1584db7c6f94e530b8a31749c82dc16b34
kyhlsd <kmyghn@gmail.com>
2024-04-19 21:01:35 +0900

    enum PrevOperator 삭제, enum PrevPressed에 equal 따로 구별

--- Project1_lab5/ContentView.swift
+++ Project1_lab5/ContentView.swift
@@ -10,20 +10,13 @@ import SwiftUI
 enum PrevPressed {
     case number
     case inputOperator
-}
-
-enum PrevOperator {
-    case plus
-    case minus
-    case devide
-    case multiply
     case equal
 }
 
 struct ContentView: View {
     @State private var text: String = "0"
     @State private var prevPressed: PrevPressed = .inputOperator
-    @State private var prevOperator: PrevOperator = .plus
+    @State private var prevOperator: String = "+"
     @State private var lhs = 0
     @State private var rhs = 0
     let buttons = [
@@ -44,21 +37,15 @@ struct ContentView: View {
                     ForEach(row, id: \.self) { buttonChar in
                         Button(buttonChar){
                             switch buttonChar {
-                            case "/" :
-                                pressOperator(inputOperator: .devide)
-                            case "*" :
-                                pressOperator(inputOperator: .multiply)
-                            case "+" :
-                                pressOperator(inputOperator: .plus)
-                            case "-" :
-                                pressOperator(inputOperator: .minus)
+                            case "/", "*", "+", "-" :
+                                pressOperator(inputOperator: buttonChar)
                             case "C":
                                 {
                                     text = "0"
                                     lhs = 0
                                     rhs = 0
                                     prevPressed = .inputOperator
-                                    prevOperator = .plus
+                                    prevOperator = "+"
                                 } ()
                             default :
                                 pressNumber(number: Int(buttonChar) ?? 0)
@@ -72,7 +59,7 @@ struct ContentView: View {
                 }
             }
             Button("=") {
-                pressOperator(inputOperator: .equal)
+                pressOperator(inputOperator: "=")
             }
             .font(.largeTitle)
             .foregroundColor(.black)
@@ -91,66 +78,49 @@ struct ContentView: View {
             if text == "0" {
                 text = ""
             }
-            if prevOperator == .equal {
-                print("새로운 계산을 하려면 초기화하세요")
-            } else {
                 rhs = number
                 text += String(number)
                 prevPressed = .number
-            }
+        case .equal:
+            print("새로운 계산을 하려면 초기화하세요")
         }
     }
     
-    func pressOperator(inputOperator: PrevOperator) -> Void {
+    func pressOperator(inputOperator: String) -> Void {
         switch prevPressed {
         case .number:
             switch prevOperator {
-            case .plus:
+            case "+":
                 lhs = lhs + rhs
-            case .minus:
+            case "-":
                 lhs = lhs - rhs
-            case .devide:
+            case "/":
                 lhs = lhs / rhs
-            case .multiply:
+            case "*":
                 lhs = lhs * rhs
-            case .equal:
+            case "=":
+                print("error")
+            default:
                 print("error")
             }
             switch inputOperator {
-            case .plus:
-                text += "+"
-            case .minus:
-                text += "-"
-            case .devide:
-                text += "/"
-            case .multiply:
-                text += "*"
-            case .equal:
+            case "+", "-", "/", "*":
+                text += String(inputOperator)
+            case "=":
                 text = String(lhs)
+            default:
+                print("error")
             }
             prevOperator = inputOperator
-            prevPressed = .inputOperator
+            prevPressed = inputOperator == "=" ? .equal : .inputOperator
         case .inputOperator:
-            switch prevOperator {
-            case .equal:
-                switch inputOperator {
-                case .plus:
-                    text += "+"
-                case .minus:
-                    text += "-"
-                case .devide:
-                    text += "/"
-                case .multiply:
-                    text += "*"
-                case .equal:
-                    text = String(lhs)
-                }
+            print("Wrong Input")
+        case .equal:
+            if inputOperator != "=" {
+                text += inputOperator
                 prevOperator = inputOperator
                 prevPressed = .inputOperator
-            default:
-                print("Wrong Input")
             }
-            print("Wrong Input")
         }
     }
 }
