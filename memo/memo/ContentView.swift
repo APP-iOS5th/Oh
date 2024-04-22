241ab95132551c6a2eec0bf85f54985a73498d7d
wotjs020708 <wotjs020708@gmail.com>
2024-04-22 15:29:08 +0900

    ContentView 구현

--- memo/memo/ContentView.swift
+++ memo/memo/ContentView.swift
@@ -9,14 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var memoStore: MemoStore = MemoStore()
+    
+    @State var isSheetShowing: Bool = false
+    @State var memoText: String = ""
+    @State var memoColor: Color = .blue
+    let colors: [Color] = [.blue, .cyan, .purple, .yellow, .indigo]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(memoStore.memos) { memo in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(memo.text)").font(.title)
+                        Text("\(memo.createdString)").font(.body).padding(.top)
+                    }
+                    Spacer()
+                }
+                .padding()
+                .foregroundStyle(.white)
+                .background(memo.color)
+                .shadow(radius: 3)
+                .padding()
+                .contextMenu{
+                    ShareLink(item: memo.text)
+                    Button{
+                        memoStore.removeMemo(memo)
+                    } label: {
+                        Image(systemName: "trash.slash")
+                        Text("삭제")
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("mememo")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("추가") {
+                        memoText = ""; isSheetShowing = true
+                    }
+                }
+            }.sheet(isPresented: $isSheetShowing) {
+                MemoAddView(memoStore: memoStore, isSheetShowing: $ isSheetShowing, memoText: $memoText, memoColor: $memoColor, colors: colors)
+            }
         }
-        .padding()
     }
 }
 
