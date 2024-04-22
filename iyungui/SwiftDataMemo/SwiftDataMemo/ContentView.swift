65f3e137e9a86b634851a82a751537cd2ff13f44
iyungui <rsdbddml@gmail.com>
2024-04-22 14:52:19 +0900

    init project-memo

---
+++ iyungui/SwiftDataMemo/SwiftDataMemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftDataMemo
+//
+//  Created by 이융의 on 4/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iyungui/SwiftDataMemo/SwiftDataMemo/SwiftDataMemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftDataMemoApp.swift
+//  SwiftDataMemo
+//
+//  Created by 이융의 on 4/22/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftDataMemoApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
