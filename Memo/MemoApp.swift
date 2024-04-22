3b2c4566534a849a215dbeba21523671715dc805
yehjinjang <j95033176@gmail.com>
2024-04-22 14:47:51 +0900

    Initial Commit

---
+++ Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by 장예진 on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
