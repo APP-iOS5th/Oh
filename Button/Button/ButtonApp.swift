1621913b20b788e408ede1ce3aa0cc2bd00dbc6e
yehjinjang <j95033176@gmail.com>
2024-04-23 14:26:15 +0900

    0423

---
+++ Button/Button/ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by 장예진 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Button/Button/PasteButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  PasteButtonView.swift
+//  Button
+//
+//  Created by 장예진 on 4/23/24.
+//
+
+import SwiftUI
+
+struct PasteButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PasteButtonView()
+}
