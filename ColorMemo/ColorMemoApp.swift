41c5de53d69a8bf09b7d065ecea7070a2f4fe51b
yehjinjang <j95033176@gmail.com>
2024-04-23 09:29:54 +0900

    Initial Commit

---
+++ ColorMemo/ColorMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMemoApp.swift
+//  ColorMemo
+//
+//  Created by 장예진 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
