f51aaa726afa70955aae98e8db7c202f1544a8e0
kyhlsd <kmyghn@gmail.com>
2024-04-25 13:04:14 +0900

    todolist iniailize project

---
+++ ToDoListApp/ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by 김영훈 on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
