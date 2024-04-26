0ed007b72ba0547c82cd4e34a2e9e0a59ed1289b
kyhlsd <kmyghn@gmail.com>
2024-04-26 10:31:48 +0900

    app group 추가

---
+++ yhkim/ToDoListApp/TodoWidget/TodoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TodoWidgetBundle.swift
+//  TodoWidget
+//
+//  Created by 김영훈 on 4/26/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TodoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TodoWidget()
+    }
+}
