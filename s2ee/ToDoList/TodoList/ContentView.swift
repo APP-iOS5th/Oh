04fb3e32dc668c8e0cc8fd2fb970c88f751ff057
s2ee <danwater86@gmail.com>
2024-04-26 16:22:54 +0900

    clean + widget

---
+++ s2ee/ToDoList/TodoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by Mac on 4/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+      
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ s2ee/ToDoList/TodoWidget/TodoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TodoWidgetBundle.swift
+//  TodoWidget
+//
+//  Created by Mac on 4/26/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TodoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TodoWidget()
+    }
+}
