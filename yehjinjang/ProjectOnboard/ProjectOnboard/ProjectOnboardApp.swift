a394f1ced418795f6aeb1353afba82d304201db1
yehjinjang <j95033176@gmail.com>
2024-04-29 23:46:55 +0900

    RegisterationView update

--- yehjinjang/ProjectOnboard/ProjectOnboard/ProjectOnboardApp.swift
+++ yehjinjang/ProjectOnboard/ProjectOnboard/ProjectOnboardApp.swift
@@ -5,13 +5,24 @@
 //  Created by 장예진 on 4/29/24.
 //
 
+//import SwiftUI
+//
+//@main
+//struct ProjectOnboardApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            OnboardingView()
+//        }
+//    }
+//}
+
 import SwiftUI
 
 @main
 struct ProjectOnboardApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RegistrationView()
         }
     }
 }
