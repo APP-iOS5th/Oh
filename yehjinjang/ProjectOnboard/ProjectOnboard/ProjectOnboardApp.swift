2a88262254efb8efe21b6b52fbb664d4a2f0a47f
yehjinjang <j95033176@gmail.com>
2024-04-29 21:29:54 +0900

    delete

---
+++ yehjinjang/ProjectOnboard/ProjectOnboard/ProjectOnboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOnboardApp.swift
+//  ProjectOnboard
+//
+//  Created by 장예진 on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOnboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
