26ead3404d3af2be74b81d0d4719e3e06ad776d0
yehjinjang <j95033176@gmail.com>
2024-04-29 22:31:23 +0900

    RemoteConfig

---
+++ yehjinjang/RemoteConfig/RemoteConfig/RemoteConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteConfigApp.swift
+//  RemoteConfig
+//
+//  Created by 장예진 on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct RemoteConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
