cf0826d3c6e89b263003bba60877ba4f103e3aad
yehjinjang <j95033176@gmail.com>
2024-04-29 22:56:52 +0900

    firebase no infoPlst

---
+++ yehjinjang/FirebaseBasic/FirebaseBasic/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  FirebaseBasic
+//
+//  Created by 장예진 on 4/29/24.
+//
+
+
+import SwiftUI
+import GoogleSignInSwift
+
+struct ContentView: View {
+    @StateObject var authenticationViewModel = AuthenticationViewModel()
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea(.all)
+            switch authenticationViewModel.state {
+            case .busy:
+                ProgressView()
+            case .signedIn:
+                ProfileView(authenticationViewModel: authenticationViewModel)
+            case .signedOut:
+                GoogleSignInButton(action: authenticationViewModel.login)
+                    .frame(width: 150, height: 30, alignment: .center)
+            }
+        }
+        .task {
+            authenticationViewModel.restorePreviousSignIn()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yehjinjang/FirebaseBasic/FirebaseBasic/FirebaseBasicApp.swift
@@ -0,0 +1,31 @@
+//
+//  FirebaseBasicApp.swift
+//  FirebaseBasic
+//
+//  Created by 장예진 on 4/29/24.
+//
+
+import SwiftUI
+import Firebase
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        GIDSignIn.sharedInstance.handle(url)
+    }
+}
+
+@main
+struct FirebaseBasicApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
