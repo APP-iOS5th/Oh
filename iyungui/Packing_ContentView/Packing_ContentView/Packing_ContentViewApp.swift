91a1e01fdbbd9662aac030da8fe8a16630b4e6d3
iyungui <rsdbddml@gmail.com>
2024-04-30 10:26:19 +0900

    Initialize Project Packing_ContentView

---
+++ iyungui/Packing_ContentView/Packing_ContentView/Packing_ContentViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Packing_ContentViewApp.swift
+//  Packing_ContentView
+//
+//  Created by 이융의 on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Packing_ContentViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
